debd7bbc3f7550f078490ef788ea815036569815
Jameson Lee <Jameson.Lee@mixpanel.com>
2017-07-28 16:04:26 -0700

    add support for tags in List() + tests

--- pingdom/api_responses.go
+++ pingdom/api_responses.go
@@ -19,26 +19,33 @@ type PingdomError struct {
 
 // CheckResponse represents the json response for a check from the Pingdom API
 type CheckResponse struct {
-	ID                       int               `json:"id"`
-	Name                     string            `json:"name"`
-	Resolution               int               `json:"resolution,omitempty"`
-	SendToAndroid            bool              `json:"sendtoandroid,omitempty"`
-	SendToEmail              bool              `json:"sendtoemail,omitempty"`
-	SendToIPhone             bool              `json:"sendtoiphone,omitempty"`
-	SendToSms                bool              `json:"sendtosms,omitempty"`
-	SendToTwitter            bool              `json:"sendtotwitter,omitempty"`
-	SendNotificationWhenDown int               `json:"sendnotificationwhendown,omitempty"`
-	NotifyAgainEvery         int               `json:"notifyagainevery,omitempty"`
-	NotifyWhenBackup         bool              `json:"notifywhenbackup,omitempty"`
-	Created                  int64             `json:"created,omitempty"`
-	Hostname                 string            `json:"hostname,omitempty"`
-	Status                   string            `json:"status,omitempty"`
-	LastErrorTime            int64             `json:"lasterrortime,omitempty"`
-	LastTestTime             int64             `json:"lasttesttime,omitempty"`
-	LastResponseTime         int64             `json:"lastresponsetime,omitempty"`
-	Paused                   bool              `json:"paused,omitempty"`
-	ContactIds               []int             `json:"contactids,omitempty"`
-	Type                     CheckResponseType `json:"type,omitempty"`
+	ID                       int                `json:"id"`
+	Name                     string             `json:"name"`
+	Resolution               int                `json:"resolution,omitempty"`
+	SendToAndroid            bool               `json:"sendtoandroid,omitempty"`
+	SendToEmail              bool               `json:"sendtoemail,omitempty"`
+	SendToIPhone             bool               `json:"sendtoiphone,omitempty"`
+	SendToSms                bool               `json:"sendtosms,omitempty"`
+	SendToTwitter            bool               `json:"sendtotwitter,omitempty"`
+	SendNotificationWhenDown int                `json:"sendnotificationwhendown,omitempty"`
+	NotifyAgainEvery         int                `json:"notifyagainevery,omitempty"`
+	NotifyWhenBackup         bool               `json:"notifywhenbackup,omitempty"`
+	Created                  int64              `json:"created,omitempty"`
+	Hostname                 string             `json:"hostname,omitempty"`
+	Status                   string             `json:"status,omitempty"`
+	LastErrorTime            int64              `json:"lasterrortime,omitempty"`
+	LastTestTime             int64              `json:"lasttesttime,omitempty"`
+	LastResponseTime         int64              `json:"lastresponsetime,omitempty"`
+	Paused                   bool               `json:"paused,omitempty"`
+	ContactIds               []int              `json:"contactids,omitempty"`
+	Type                     CheckResponseType  `json:"type,omitempty"`
+	Tags                     []CheckResponseTag `json:"tags,omitempty"`
+}
+
+type CheckResponseTag struct {
+	Name  string `json:"name"`
+	Type  string `json:"type"`
+	Count int    `json:"count"`
 }
 
 type CheckResponseType struct {
