9c0b70494a5d08bf77e97e34b2929cb7a1d58f78
Russell Cardullo <russellcardullo@gmail.com>
2018-04-21 08:18:43 -0700

    Filter zero valued sendnotificationswhendown
    Pingdom does not except a zero value for this field.
    Since this is the default for the struct, ensure
    we don't send the value to the Pingdom API.

--- pingdom/check_types.go
+++ pingdom/check_types.go
@@ -50,26 +50,29 @@ type PingCheck struct {
 // with an HTTP PUT request
 func (ck *HttpCheck) PutParams() map[string]string {
 	m := map[string]string{
-		"name":                     ck.Name,
-		"host":                     ck.Hostname,
-		"resolution":               strconv.Itoa(ck.Resolution),
-		"paused":                   strconv.FormatBool(ck.Paused),
-		"sendnotificationwhendown": strconv.Itoa(ck.SendNotificationWhenDown),
-		"notifyagainevery":         strconv.Itoa(ck.NotifyAgainEvery),
-		"notifywhenbackup":         strconv.FormatBool(ck.NotifyWhenBackup),
-		"url":                      ck.Url,
-		"encryption":               strconv.FormatBool(ck.Encryption),
-		"postdata":                 ck.PostData,
-		"integrationids":           intListToCDString(ck.IntegrationIds),
-		"tags":                     ck.Tags,
-		"probe_filters":            ck.ProbeFilters,
-		"userids":                  intListToCDString(ck.UserIds),
-		"teamids":                  intListToCDString(ck.TeamIds),
+		"name":             ck.Name,
+		"host":             ck.Hostname,
+		"resolution":       strconv.Itoa(ck.Resolution),
+		"paused":           strconv.FormatBool(ck.Paused),
+		"notifyagainevery": strconv.Itoa(ck.NotifyAgainEvery),
+		"notifywhenbackup": strconv.FormatBool(ck.NotifyWhenBackup),
+		"url":              ck.Url,
+		"encryption":       strconv.FormatBool(ck.Encryption),
+		"postdata":         ck.PostData,
+		"integrationids":   intListToCDString(ck.IntegrationIds),
+		"tags":             ck.Tags,
+		"probe_filters":    ck.ProbeFilters,
+		"userids":          intListToCDString(ck.UserIds),
+		"teamids":          intListToCDString(ck.TeamIds),
 	}
 
-	// Ignore port is not defined
+	// Ignore zero values
 	if ck.Port != 0 {
 		m["port"] = strconv.Itoa(ck.Port)
+	}
+
+	if ck.SendNotificationWhenDown != 0 {
+		m["sendnotificationwhendown"] = strconv.Itoa(ck.SendNotificationWhenDown)
 	}
 
 	// ShouldContain and ShouldNotContain are mutually exclusive.
